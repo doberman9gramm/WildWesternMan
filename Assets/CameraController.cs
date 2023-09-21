9681d1c021cd03abc4ebce0b3e0928441316a2ac
Виктор Перов <73wwz@bk.ru>
2023-09-21 18:52:06 +0300

    Настройка камеры, изменение мешов

---
+++ Assets/CameraController.cs
@@ -0,0 +1,50 @@
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CameraController : MonoBehaviour
+{
+    private CinemachineVirtualCamera _cinemachineVirtualCamera;
+    private CinemachineFramingTransposer _cinemachineFramingTransposer;
+
+    [Header("Distance")]
+    [SerializeField] float _minDistance = 0f;
+    [SerializeField] float _maxDistance = 45f;
+
+    [Header("Angle")]
+    [SerializeField] float _minAngle = 0f;
+    [SerializeField] float _maxAngle = 45f;
+
+    private void Awake()
+    {
+        _cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        _cinemachineFramingTransposer = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+    }
+
+    public void ChangeCameraDistance(float value)
+    {
+        ChangeAngle(value);
+        value += _cinemachineFramingTransposer.m_CameraDistance;
+
+        if (value < _minDistance)
+            value = _minDistance;
+
+        if (value > _maxDistance)
+            value = _maxDistance;
+
+        _cinemachineFramingTransposer.m_CameraDistance = value;
+    }
+
+    private void ChangeAngle(float value)
+    {
+        value = transform.eulerAngles.x + value;
+
+        if (value < _minAngle)
+            value = _minAngle;
+
+        if (value > _maxDistance)
+            value = _maxDistance;
+
+        transform.eulerAngles = new Vector3(value, 0 ,0);
+    }
+}
