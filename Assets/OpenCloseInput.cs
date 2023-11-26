4b7bcb875c286c1ba949705d8691a87fa873f1de
Виктор Перов <73wwz@bk.ru>
2023-11-26 22:43:35 +0300

    Работа с анимациями

--- Assets/OpenCloseInput.cs
+++ Assets/OpenCloseInput.cs
@@ -6,12 +6,13 @@ public class OpenCloseInput : MonoBehaviour
     [SerializeField] private InputActionReference _input;
     [SerializeField] private GameObject _gameObject;
 
+    private void OnEnable()
+    {
+        
+    }
+
     private void Update()
     {
-        if (_input.action.ReadValue<float>() != 0)
-        {
-            Debug.Log("True");
-            _gameObject.SetActive(!_gameObject.activeSelf);
-        }
+        _gameObject.SetActive(!_gameObject.activeSelf);
     }
 }
